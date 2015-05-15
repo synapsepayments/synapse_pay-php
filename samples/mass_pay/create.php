200ec9d1a1b0b68f19da3cd31e1ccba855ef5375
Jon Calhoun <joncalhoun@gmail.com>
2015-05-15 15:43:59 -0400

    updatin gthe mass pay create example with real data

--- samples/mass_pay/create.php
+++ samples/mass_pay/create.php
@@ -8,7 +8,60 @@ require(dirname(__FILE__) . '/../../init.php');
 
 $client = \SynapsePay\User::login( "3ac38d63db58466982fe6f871c48f1", "TestTest123$" );
 
+// With bank info
 $massPays = $client->massPays->create([
-  "mass_pays" => "mass pay objects here...",
+  "mass_pays" => [
+      [
+        "legal_name" => "Some Person 1",
+        "account_number" => "888888888",
+        "routing_number" => "222222222",
+        "amount" => "10.33",
+        "trans_type" => "0",
+        "account_class" => "1",
+        "account_type" => "2",
+        "user_info" => [
+          "email" => "some@email.com",
+          "phone_number" => "9011234567",
+          "ip_address" => "some.ip.address",
+          "dob" => "18/11/1989",
+          "risk_score" => 10
+        ]
+      ],
+      [
+        "legal_name" => "Some Person 2",
+        "account_number" => "888888888",
+        "routing_number" => "222222222",
+        "amount" => "10.33",
+        "trans_type" => "0",
+        "account_class" => "1",
+        "account_type" => "1"
+      ]
+    ],
 ]);
-var_dump($massPays);
+
+foreach( $massPays->data as $massPay ) {
+  echo "Mass Pay:: ";
+  var_dump( $massPay );
+}
+
+
+// With cards instead of bank info
+$massPays = $client->massPays->create([
+  "mass_pays" => [
+      [
+        "amount" => "20",
+        "trans_type" => "0",
+        "card_id" => "77"
+      ],
+      [
+        "amount" => "20",
+        "trans_type" => "0",
+        "card_id" => "76"
+      ]
+    ],
+]);
+
+foreach( $massPays->data as $massPay ) {
+  echo "Mass Pay:: ";
+  var_dump( $massPay );
+}
